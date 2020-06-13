b8d09b425c8d414a0781656a1d833fcb0fc07ffc
Johnatan Boteor <johnatan@jbotero.com>
2020-06-13 11:08:03 -0500

    format

--- App/Screens/Cart/index.js
+++ App/Screens/Cart/index.js
@@ -26,4 +26,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Content);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Content);
