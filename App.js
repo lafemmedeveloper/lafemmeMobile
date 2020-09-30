2adb6563fa8d972b170179bbaf7bd39c3b4d66f3
John Doe <jesusebzcp19@gmail.com>
2020-09-29 22:15:26 -0500

    add package netinfo

--- App.js
+++ App.js
@@ -3,12 +3,19 @@ import DeviceInfo from 'react-native-device-info';
 import Router from './src/config/Router';
 import RouterExpert from './src/config/RouterExpert';
 import Store from './src/flux';
+import NetInfo from '@react-native-community/netinfo';
 
 let bundleId = DeviceInfo.getBundleId();
 console.disableYellowBox = true;
+
 const App = () => {
   const isClient =
     bundleId === 'com.femme.client' || bundleId === 'com.femme.clientstaging';
+
+  NetInfo.fetch().then((state) => {
+    console.log('Connection type ====>', state.type);
+    console.log('Is connected? ====>', state.isConnected);
+  });
   return <Store>{isClient ? <Router /> : <RouterExpert />}</Store>;
 };
 
